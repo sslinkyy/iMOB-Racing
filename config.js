0d15077bc252b2fbf8e01a8a182d3b43720c7842
sslinkyy <ayorba@gmail.com>
2024-06-10 22:07:11 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
-    YOUTUBE_API_KEY: 'YOUR_YOUTUBE_API_KEY',
+    YOUTUBE_API_KEY: 'AIzaSyDaCJQhs3fz-k7hg-j0NQWj0S1r7ZEvThs',
     CHANNEL_ID: 'UCbvXNK-13KBK_yZuZ5YeLZw'
 };
