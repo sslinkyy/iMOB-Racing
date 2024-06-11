1e6c333ce165889368b4c9abc3b4a6130b2032c2
sslinkyy <ayorba@gmail.com>
2024-06-10 23:15:21 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // config.js
 const CONFIG = {
     YOUTUBE_API_KEY: 'AIzaSyDaCJQhs3fz-k7hg-j0NQWj0S1r7ZEvThs',
-    CHANNEL_ID: 'UCbvXNK-13KBK_yZuZ5YeLZw',
+    YOUTUBE_CHANNEL_ID: 'UCbvXNK-13KBK_yZuZ5YeLZw',
     FACEBOOK_PAGE_URL: 'https://www.facebook.com/imobracing',
     GOOGLE_FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSfZqtnqgL2im8K7Ih6hCjqLrEZ-frZzTMMlJLbMCMg4u8MxaA/viewform?usp=sf_link',
     DISQUS_SHORTNAME: 'imobracing',
     DISQUS_PAGE_URL: 'https://imobracingonline.com',
-    DISQUS_PAGE_IDENTIFIER: 'imobracing-forums'
-    GOOGLE_CALENDAR_ID: 'https://calendar.google.com/calendar/embed?src=imobracing%40gmail.com&ctz=America%2FLos_Angeles'
+    DISQUS_PAGE_IDENTIFIER: 'imobracing-forums',
+    GOOGLE_CALENDAR_ID: 'https://calendar.google.com/calendar/embed?src=imobracing%40gmail.com&ctz=America%2FLos_Angeles',
 };
