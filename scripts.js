c7342c8429042fb505178a361cb55f4d78200a46
sslinkyy <ayorba@gmail.com>
2024-06-12 01:35:17 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -49,9 +49,6 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(section);
     });
 
-    // Initialize Firebase
-    firebase.initializeApp(CONFIG.FIREBASE_CONFIG);
-
     // YouTube Data API Key
     const API_KEY = CONFIG.YOUTUBE_API_KEY;
     const CHANNEL_ID = CONFIG.YOUTUBE_CHANNEL_ID;
